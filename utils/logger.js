c86d34262ee2a91a06ae392c4f2b9eccfaa018d0
Maxime Suard <maxime.suard@gmail.com>
2018-05-22 14:55:07 +0100

    add logger.js

---
+++ utils/logger.js
@@ -0,0 +1,40 @@
+const { createLogger, format, transports } = require('winston');
+require('winston-syslog').Syslog; // eslint-disable-line no-unused-expressions
+const path = require('path');
+const packageJson = require('../package.json');
+
+const appDir = path.dirname(require.main.filename);
+
+const {
+  combine,
+  timestamp,
+  printf,
+  colorize,
+} = format;
+
+const formatter = printf((info) => {
+  const output = `${info.timestamp} [${packageJson.name}/${packageJson.version}] ${info.level}: ${info.message}`;
+  return output;
+});
+
+const logger = createLogger({
+  transports: [
+    new transports.Console({
+      level: 'debug',
+      format: combine(
+        colorize(),
+        timestamp(),
+        formatter,
+      ),
+    }),
+    new transports.Syslog(),
+    new transports.File({ level: 'silly', filename: `${appDir}/logs/${packageJson.name}.log` }),
+    new transports.File({ level: 'debug', filename: `${appDir}/logs/${packageJson.name}-debug.log` }),
+    new transports.File({ level: 'verbose', filename: `${appDir}/logs/${packageJson.name}-verbose.log` }),
+    new transports.File({ level: 'info', filename: `${appDir}/logs/${packageJson.name}-info.log` }),
+    new transports.File({ level: 'warn', filename: `${appDir}/logs/${packageJson.name}-warn.log` }),
+    new transports.File({ level: 'error', filename: `${appDir}/logs/${packageJson.name}-error.log` }),
+  ],
+});
+
+module.exports = logger;
