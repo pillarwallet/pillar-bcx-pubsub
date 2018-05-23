9185d6070888348ae1eb2d8c9efe9bd996d5ef86
Rafael Cidade <rafael@2030.io>
2018-05-23 17:48:10 +0100

    addresses scheme

--- src/models/accounts_model.js
+++ src/models/accounts_model.js
@@ -2,11 +2,12 @@ const Mongoose = require('../services/dbServices.js').mongoose;
 
 const accountsSchema = Mongoose.Schema({
   pillarId: { type: Number, required: true },
-  addresses: {
-    ethereum : { type: String, required: true },
-    bitcoin : { type: String, required: true },
-    litecoin : { type: String, required: true }
-  }
+  addresses: [
+    {
+      protocol: String,
+      walletId: String 
+    }
+  ]
 });
 
 const accounts = Mongoose.model('Accounts', accountsSchema);
