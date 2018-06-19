5ca6ffa48605830cbb4ea0b71b02b012c8c07365
Rafael Cidade <rafael@2030.io>
2018-06-19 15:05:15 +0100

    variable name

--- src/master.launch.test.js
+++ src/master.launch.test.js
@@ -2,28 +2,28 @@ var child_process = require('child_process');
 const logger = require('./utils/logger');
 logger.transports.forEach((t) => (t.silent = true));
 
-var mockRequest = {fork: () => {}};
+var mockChild = {fork: () => {}};
 var master;
 
 describe("Method: master.launch()", () => {
     
     beforeEach(() => {
-        mockRequest.fork = jest.fn();
+        mockChild.fork = jest.fn();
         jest.mock("child_process", () => {
-            return mockRequest;
+            return mockChild;
             });
     });
 
     afterEach(() => {
-        mockRequest.fork = child_process.fork;
+        mockChild.fork = child_process.fork;
     });
 
     it('Expect master.launch() to call fork() with: housekeeper, publisher, and subscriber', () => {
         master = require('./master');
         master.launch();
-        expect(mockRequest.fork).toBeCalled();
-        expect(mockRequest.fork).toBeCalledWith(`${__dirname}/housekeeper.js`);
-        expect(mockRequest.fork).toBeCalledWith(`${__dirname}/publisher.js`);
-        expect(mockRequest.fork).toBeCalledWith(`${__dirname}/subscriber.js`);
+        expect(mockChild.fork).toBeCalled();
+        expect(mockChild.fork).toBeCalledWith(`${__dirname}/housekeeper.js`);
+        expect(mockChild.fork).toBeCalledWith(`${__dirname}/publisher.js`);
+        expect(mockChild.fork).toBeCalledWith(`${__dirname}/subscriber.js`);
     });
 });
