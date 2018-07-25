ad7c181768800570c4765816fa56e840459ddbd0
Rafael Cidade <rafael@2030.io>
2018-07-25 14:07:32 +0100

    subscriber unit tests

--- src/subscriber.test.js
+++ src/subscriber.test.js
@@ -1,24 +1,29 @@
-const sinon = require('sinon');
+const dbServices = require('./services/dbServices.js');
+const rmqServices = require('./services/rmqServices.js');
+var subscriber = require("./subscriber.js");
+var amqp = require('amqplib/callback_api');
+const logger = require('./utils/logger');
+logger.transports.forEach((t) => (t.silent = true));
+require('dotenv').config();
+ 
+describe('Function Calls', () => {
 
-describe('Test function calls', () => {
-	test('Expect dbConnect and initPubSubMQto be called by initServices()', (done) => {
-		const dbServices = require('./services/dbServices.js');
-		const stub = sinon.stub(dbServices, 'dbConnect');
-		stub.resolves();
+  it('Expect dbConnect to be called by initServices()', () => {
 
-		const rmqServices = require('./services/rmqServices.js');
-		const stub2 = sinon.stub(rmqServices, 'initSubPubMQ');
-		stub2.resolves();
+    const spyConnect = jest.spyOn(dbServices, 'dbConnect').mockImplementation(() => Promise.resolve({ success: true }));
+    subscriber.initServices();
+    expect(spyConnect).toBeCalled();
+    spyConnect.mockRestore() 
+  });
 
-		const subscriber = require('./subscriber.js')
-		subscriber.initServices()
-		.then(() => {
-			sinon.assert.calledTwice(stub);
-			sinon.assert.calledTwice(stub2);
-			stub.restore();
-			stub2.restore();
-			done();
-		})
+  it('Expect amqp.connect to be called by initSubPubMQ()', () => {
 
-	});
+    const spyConnect = jest.spyOn(amqp, 'connect');
+		rmqServices.initSubPubMQ();
+		expect(spyConnect).toBeCalled();
+		const rmqUrl = "amqp://" + process.env.MQ_BCX_USERNAME + ":" + process.env.MQ_BCX_PASSWORD + "@" + process.env.RABBITMQ_SERVER;
+        expect(spyConnect).toBeCalledWith(rmqUrl, expect.anything());
+        spyConnect.mockRestore();
+  });
 });
+
