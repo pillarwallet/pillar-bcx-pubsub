d7ae9dcc2e04421cfd3222d3f222fa2fc31887d3
Paul Thethi <gurpal.thethi@gmail.com>
2018-08-07 16:18:09 +0100

    Modified subscriber test.

--- src/subscriber.test.js
+++ src/subscriber.test.js
@@ -1,25 +1,11 @@
-const sinon = require('sinon');
-const logger = require('./utils/logger');
-logger.transports.forEach((t) => (t.silent = true));
+const subscriber = require('./subscriber.js');
 
 describe('Test function calls', () => {
-	test('Expect dbConnect and initPubSubMQ to be called by initServices()', (done) => {
-		const dbServices = require('./services/dbServices.js');
-		const stub = sinon.stub(dbServices, 'dbConnect');
-		stub.resolves();
+  test('Expect initServices() to be called', () => {
+    const spy = jest.spyOn(subscriber, 'initServices');
+    spy.mockImplementation();
+    spy.call();
 
-		const rmqServices = require('./services/rmqServices.js');
-		const stub2 = sinon.stub(rmqServices, 'initSubPubMQ');
-		stub2.resolves();
-
-		const subscriber = require('./subscriber.js')
-		subscriber.initServices()
-		.then(() => {
-			sinon.assert.calledTwice(stub);
-			sinon.assert.calledTwice(stub2);
-			stub.restore();
-			stub2.restore();
-			done();
-		});
-	});
+    expect(spy).toHaveBeenCalled();
+  });
 });
