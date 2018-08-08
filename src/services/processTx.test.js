e3d954bae1f4730810ca822d17993d37fa36c92a
Rafael Cidade <rafael@2030.io>
2018-08-08 13:44:55 +0100

    adding new processTx tests

--- src/services/processTx.test.js
+++ src/services/processTx.test.js
@@ -1,6 +1,6 @@
 const processTx = require('./processTx');
 
-describe.only('processTx module', () => {
+describe.only('The processTx module', () => {
   describe('The storeTransactionStats function tests', () => {
     it('should return true', () => {
 		const spy = jest.spyOn(processTx, 'storeTransactionStats');
@@ -21,7 +21,7 @@ describe.only('processTx module', () => {
     });
   });
 
-  describe('The connect function tests', () => {
+  describe('The storeTokenEvent function tests', () => {
     it('should return true', () => {
 		const spy = jest.spyOn(processTx, 'storeTokenEvent');
 		const ret = true;
@@ -37,8 +37,79 @@ describe.only('processTx module', () => {
     	const ret = false;
 
 		spy.mockImplementation((event,asset,protocol,txn) => ret);
-		expect(processTx.storeTokenEvent()).toEqual(ret);
+		expect(processTx.storeTokenEvent('arg1', 'arg2', 'arg3', 'arg4')).toEqual(ret);
+		expect(spy).toBeCalledWith('arg1', 'arg2', 'arg3', 'arg4');
 		spy.mockRestore();
-    });
-  });
+	});
 });
+	
+	describe('The storeIfRelevant function tests', () => { 
+		it('should return true', () => {
+			const spy = jest.spyOn(processTx, 'storeIfRelevant');
+			const ret = true;
+	
+			spy.mockImplementation((tx, protocol) => ret);
+			expect(processTx.storeIfRelevant('arg1', 'arg2')).toEqual(ret);
+			expect(spy).toBeCalledWith('arg1', 'arg2');
+			spy.mockRestore();
+		});
+
+		it('should return false', () => {
+			const spy = jest.spyOn(processTx, 'storeIfRelevant');
+			const ret = false;
+	
+			spy.mockImplementation((tx, protocol) => ret);
+			expect(processTx.storeIfRelevant('arg1', 'arg2')).toEqual(ret);
+			expect(spy).toBeCalledWith('arg1', 'arg2');
+			spy.mockRestore();
+		});
+	});
+
+	describe('The newPendingTran function tests', () => { 
+		it('should return true', () => {
+			const spy = jest.spyOn(processTx, 'newPendingTran');
+			const ret = true;
+	
+			spy.mockImplementation((tx, protocol) => ret);
+			expect(processTx.newPendingTran('arg1', 'arg2')).toEqual(ret);
+			expect(spy).toBeCalledWith('arg1', 'arg2');
+			spy.mockRestore();
+		});
+
+		it('should return false', () => {
+			const spy = jest.spyOn(processTx, 'newPendingTran');
+			const ret = false;
+	
+			spy.mockImplementation((tx, protocol) => ret);
+			expect(processTx.newPendingTran('arg1', 'arg2')).toEqual(ret);
+			expect(spy).toBeCalledWith('arg1', 'arg2');
+			spy.mockRestore();
+		});
+	});
+
+	describe('The checkTokenTransfer function tests', () => { 
+		it('should return true', () => {
+			const spy = jest.spyOn(processTx, 'checkTokenTransfer');
+			const ret = true;
+	
+			spy.mockImplementation((tx, protocol) => ret);
+			expect(processTx.checkTokenTransfer('arg1', 'arg2', 'arg3')).toEqual(ret);
+			expect(spy).toBeCalledWith('arg1', 'arg2', 'arg3');
+			spy.mockRestore();
+		});
+
+		it('should return false', () => {
+			const spy = jest.spyOn(processTx, 'checkTokenTransfer');
+			const ret = false;
+	
+			spy.mockImplementation((tx, protocol) => ret);
+			expect(processTx.checkTokenTransfer('arg1', 'arg2', 'arg3')).toEqual(ret);
+			expect(spy).toBeCalledWith('arg1', 'arg2', 'arg3');
+			spy.mockRestore();
+		});
+	});
+
+});
+
+
+
