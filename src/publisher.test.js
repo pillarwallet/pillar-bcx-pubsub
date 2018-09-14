3c776efe86d9f17b20f23194e600bb5b4c4f39a5
Rafael Cidade <rafael@pillarproject.io>
2018-09-14 11:49:11 +0100

    removing logger spy

--- src/publisher.test.js
+++ src/publisher.test.js
@@ -3,21 +3,13 @@ const logger = require('./utils/logger');
 logger.transports.forEach((t) => (t.silent = true));
 
 describe('Test init functions ', () => {
-	test('Expect initIPC to call logger.info, process.send', () => {
+	test('Expect initIPC to call process.send', () => {
 		const spy = sinon.spy(process, 'send');
-		const spy2 = sinon.spy(logger, 'info');
 		const publisher = require('./publisher.js');
 		return publisher.initIPC()
 		.then(() => {
 			sinon.assert.called(spy);
-			sinon.assert.called(spy2);
-			sinon.assert.calledWith(spy2, 'Started executing publisher.initIPC()');
-			sinon.assert.calledWith(spy2, 'Publisher requesting master a list of assets to monitor');
-			sinon.assert.calledWith(spy2, 'Publisher initializing the RMQ');
-			sinon.assert.calledWith(spy2, 'Publisher polling master for new wallets every 5 seconds');
-			sinon.assert.calledWith(spy2, 'Exited publisher.initIPC()');
 			spy.restore();
-			spy2.restore();
 		});
 	});
 
