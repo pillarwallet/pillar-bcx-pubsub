9df3cd2ba50b3fefa19dae23f910f04a21de9b7a
Parthasarathy Ramanujam <partha@Parthasarathys-MacBook-Pro.local>
2018-09-26 15:08:17 +0100

    Replaced winston with log4js

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,40 +1,19 @@
-const { createLogger, format, transports } = require('winston');
-require('winston-syslog').Syslog; // eslint-disable-line no-unused-expressions
+const log4js = require('log4js');
 const path = require('path');
 const packageJson = require('../../package.json');
-
 const appDir = path.dirname(require.main.filename);
 
-const {
-  combine,
-  timestamp,
-  printf,
-  colorize,
-} = format;
-
-const formatter = printf((info) => {
-  const output = `${info.timestamp} - ${process.argv[1].substring(process.argv[1].lastIndexOf('/') + 1)} : [${packageJson.name}/${packageJson.version}] ${info.level}: ${info.message}`;
-  return output;
+log4js.configure({
+  appenders: {
+    out: { type: 'console' }, 
+    error: { type: 'dateFile', filename: `${appDir}/logs/${packageJson.name}-debug.log`, "pattern":"-dd.log",alwaysIncludePattern:true}, 
+    default: { type: 'dateFile', filename: `${appDir}/logs/${packageJson.name}-debug.log`, "pattern":"-dd.log",alwaysIncludePattern:true}
+  },
+  categories: {
+    default: { appenders: ['out','default'], level: 'debug' },
+    error: { appenders: ['error'], level: 'error' }
+  }
 });
 
-const logger = createLogger({
-  transports: [
-    new transports.Console({
-      level: 'debug',
-      format: combine(
-        colorize(),
-        timestamp(),
-        formatter,
-      ),
-    }),
-    new transports.Syslog(),
-    new transports.File({ level: 'silly', filename: `${appDir}/logs/${packageJson.name}.log` }),
-    new transports.File({ level: 'debug', filename: `${appDir}/logs/${packageJson.name}-debug.log` }),
-    new transports.File({ level: 'verbose', filename: `${appDir}/logs/${packageJson.name}-verbose.log` }),
-    new transports.File({ level: 'info', filename: `${appDir}/logs/${packageJson.name}-info.log` }),
-    new transports.File({ level: 'warn', filename: `${appDir}/logs/${packageJson.name}-warn.log` }),
-    new transports.File({ level: 'error', filename: `${appDir}/logs/${packageJson.name}-error.log` }),
-  ],
-});
-
+var logger = log4js.getLogger('syslog');
 module.exports = logger;
