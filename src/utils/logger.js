f057da0e27644af46b221d183d1b6c1243cadaee
Rafael Cidade <rafael@pillarproject.io>
2018-10-08 17:05:21 +0100

    changing to common-logger, adding dependencies

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,17 +1,12 @@
-const log4js = require('log4js');
-const path = require('path');
+require('dotenv').config();
+const buildLogger = require('@pillarwallet/common-logger');
+var appRoot = require('app-root-path');
 const packageJson = require('../../package.json');
-const appDir = path.dirname(require.main.filename);
-let logger = log4js.getLogger('syslog');
-log4js.configure({
-  appenders: {
-    out: { type: 'console' }, 
-    error: { type: 'dateFile', filename: `${appDir}/logs/${packageJson.name}-debug`, "pattern":".log",alwaysIncludePattern:true}, 
-    default: { type: 'dateFile', filename: `${appDir}/logs/${packageJson.name}-debug`, "pattern":".log",alwaysIncludePattern:true}
-  },
-  categories: {
-    default: { appenders: ['out','default'], level: 'info' },
-    error: { appenders: ['error'], level: 'error' }
-  }
-});
+const appDir = appRoot + process.env.LOGS_DIR;
+const logger = buildLogger(
+    { level: 'debug', name: packageJson.name, path: appDir }
+);
+
 module.exports = logger;
+
+
