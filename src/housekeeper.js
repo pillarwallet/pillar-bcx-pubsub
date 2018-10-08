c04b917b7daae23b3529e39f998cfd0a31b2b8ca
Parthasarathy Ramanujam <partha@Parthasarathys-MacBook-Pro.local>
2018-10-08 10:53:25 +0100

    Updated housekeeper with error handling functionality

--- src/housekeeper.js
+++ src/housekeeper.js
@@ -96,18 +96,18 @@ async function recoverWallet(walletId, pillarId, nbBlocks) {
         var status;
         var hash;
         var pillarId = '';
-        logger.info('Housekeeper.recoverWallet() - Attempting to recover transactions for ' + walletId + ' over the past ' + nbBlocks);
+        logger.info(`Housekeeper.recoverWallet() - Attempting to recover transactions for ${walletId} over the past ${nbBlocks} blocks`);
         ethService.getLastBlockNumber().then((startBlock) => {
             var endBlock = startBlock - nbBlocks;
-            logger.debug('Recovering transactions from startBlock: ' + startBlock + ' to endBlock: ' + endBlock);
+            logger.debug(`Recovering transactions from startBlock: ${startBlock} to endBlock: ${endBlock}`);
             for(var i = startBlock; i > endBlock; i--) { 
-                logger.debug('Housekeeper.recoverWallet(): Fetching transactions from block: ' + i);
+                logger.debug(`Housekeeper.recoverWallet(): Fetching transactions from block: ${i}`);
                 ethService.getBlockTx(i).then((transactions) => {
-                    logger.debug('Housekeeper.recoverWallet: Total transactions in block ' + i + ' is ' + transactions.length);
+                    logger.debug(`Housekeeper.recoverWallet: Total transactions in block ${i} is ${transactions.length}`);
                     transactions.forEach((txn) => {
-                        logger.debug('Housekeeper.recoverWallet() fetch transaction receipt for tran: ' + txn.hash);
+                        logger.debug(`Housekeeper.recoverWallet() fetch transaction receipt for tran: ${txn.hash}`);
                         ethService.getTxReceipt(txn.hash).then((receipt) => {
-                            logger.debug('Housekeeper.recoverWallet(): Validating txn hash: ' + receipt.transactionHash); 
+                            logger.debug(`Housekeeper.recoverWallet(): Validating txn hash: ${receipt.transactionHash}`); 
                             if(receipt.from === walletId || receipt.to === walletId) {
                                 from = receipt.from;
                                 to = receipt.to;
@@ -151,7 +151,7 @@ async function recoverWallet(walletId, pillarId, nbBlocks) {
                                                     blockNumber: receipt.blockNumber,
                                                     status
                                                 };
-                                                logger.debug('Housekeeper.recoverWallet(): Saving transaction into the database: ' + entry);
+                                                logger.debug(`Housekeeper.recoverWallet(): Saving transaction into the database: ${entry}`);
                                                 dbServices.dbCollections.transactions.addTx(entry);
                                             } else {
                                                 throw new Error('newTx: Transaction already exists');
@@ -164,11 +164,11 @@ async function recoverWallet(walletId, pillarId, nbBlocks) {
                     });
                 });
             }
-            logger.debug('Housekeeper.recoverWallet(): finished recovering for wallets: ' + walletId);
+            logger.debug(`Housekeeper.recoverWallet(): finished recovering for wallets: ${walletId}`);
         });
 
     }catch(e) {
-        logger.error('Housekeeper.recoverWallet(): Failed with error ' + e); 
+        logger.error(`Housekeeper.recoverWallet(): Failed with error ${e}`); 
     }
 }
 module.exports.recoverWallet = recoverWallet;
@@ -180,21 +180,21 @@ module.exports.recoverWallet = recoverWallet;
  */
 async function recoverAssetEvents(wallet,pillarId) {
     try {
-        logger.info('Housekeeper.recoverAssetEvents() started recovering asset events since last run for address: ' + wallet);
+        logger.info(`Housekeeper.recoverAssetEvents() started recovering asset events since last run for address: ${wallet}`);
         dbServices.listAssets(protocol).then((assets) => {
             assets.forEach((asset) => {
-                logger.debug('Housekeeper.recoverAssetEvents() : recoving past events of asset ' + asset.symbol);
+                logger.debug(`Housekeeper.recoverAssetEvents() : recoving past events of asset ${asset.symbol}`);
                 dbServices.findMaxBlock(protocol,asset.symbol).then((blockNumber) => {
                     if(blockNumber === undefined) {
                         blockNumber = 0;
                     }
-                    logger.debug('Housekeeper.recoverAssetEvents(): recovering since ' + blockNumber);
+                    logger.debug(`Housekeeper.recoverAssetEvents(): recovering since ${blockNumber}`);
                     ethService.getPastEvents(asset.contractAddress,'Transfer',blockNumber,wallet,pillarId);
                 });
             });
         });
     }catch(e) {
-        logger.error('Housekeeper.recoverAssetEvents() failed for address: ' + wallet + ' with error: ' + e);
+        logger.error(`Housekeeper.recoverAssetEvents() failed for address: ${wallet} with error:clear ${e}`);
     }
 }
 module.exports.recoverAssetEvents = recoverAssetEvents;
@@ -211,22 +211,39 @@ function processData(lastId) {
             //fetch new registrations since last run
             logger.info(`Housekeeper fetching new registrations after ID: ${lastId}`);
             dbServices.recentAccounts(lastId).then(async (accounts) => {
-                logger.info(`Housekeeper found accounts: ${accounts.length} NEW to process.`);
-                var cnt = 0;
-                await accounts.forEach((account) => {
-                    account.addresses.forEach(async (acc) => {
-                        if(acc.protocol == protocol) {
-                            await this.recoverWallet(acc.address,account.pillarId,LOOK_BACK_BLOCKS);
-                            await this.recoverAssetEvents(acc.address,account.pillarId);
-                            logger.info(`Finished recovering transactions for wallet: ${acc.address} index: ${cnt}`);
+                logger.info(`Housekeeper found accounts: ${accounts.length} wallets to process.`);
+                if(accounts === null || accounts.length === 0) {
+                    entry.status = 'completed';
+                    entry.endTime = time.now();
+                    client.set('housekeeper',JSON.stringify(entry), redis.print);
+                    logger.info(`Housekeeper.processData() - Completed processing ${accounts.length} records.`)
+                    this.logMemoryUsage();
+                    process.exit();
+
+                } else {
+                    var cnt =0;
+                    await accounts.forEach((account) => {
+                        cnt++;
+                        account.addresses.forEach(async (acc) => {
+                            if(acc.protocol == protocol) {
+                                await this.recoverWallet(acc.address,account.pillarId,LOOK_BACK_BLOCKS);
+                                await this.recoverAssetEvents(acc.address,account.pillarId);
+                                logger.info(`Finished recovering transactions for wallet: ${acc.address}`);
+                                entry.lastId = account._id;
+                            }
+                        });
+                        //update redis after processing
+                        if(cnt === accounts.length) {
+                            entry.status = 'completed';
                             entry.lastId = account._id;
+                            entry.endTime = time.now();
+                            client.set('housekeeper',JSON.stringify(entry), redis.print);
+                            logger.info(`Housekeeper.processData() - Completed processing ${accounts.length} records.`)
                             this.logMemoryUsage();
+                            process.exit();
                         }
                     });
-                });
-                entry.status = 'completed';
-                entry.endTime = time.now();
-                client.set('housekeeper',JSON.stringify(entry), redis.print);
+                }
             });
         });
     } catch(e) {
