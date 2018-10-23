72388284144190d1b572ff7bed63acec273fca79
Rafael Cidade <rafael@pillarproject.io>
2018-10-23 11:29:57 +0100

    common logger level env var

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -3,8 +3,9 @@ const buildLogger = require('@pillarwallet/common-logger');
 var appRoot = require('app-root-path');
 const packageJson = require('../../package.json');
 const appDir = appRoot + process.env.LOGS_DIR;
+const level = process.env.LOGS_LEVEL
 const logger = buildLogger(
-    { level: 'info', name: packageJson.name+'-info', path: appDir }
+    { level, name: packageJson.name + '-' + level, path: appDir }
 );
 
 module.exports = logger;
