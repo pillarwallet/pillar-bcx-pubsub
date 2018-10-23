68235dccd3cd1e418b10107e892ff512648aa17f
Parthasarathy Ramanujam <partha@Parthasarathys-MacBook-Pro.local>
2018-10-23 10:08:07 +0100

    Fixed paths

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,13 +1,14 @@
 const log4js = require('log4js');
 const path = require('path');
 const packageJson = require('../../package.json');
-const appDir = path.dirname(require.main.filename);
+//const appDir = path.dirname(require.main);
+const appDir = require('app-root-path');
 let logger = log4js.getLogger('syslog');
 log4js.configure({
   appenders: {
     out: { type: 'console' }, 
-    error: { type: 'fileSync', filename: `${appDir}/logs/${packageJson.name}-debug`, "pattern":".log",alwaysIncludePattern:true}, 
-    default: { type: 'fileSync', filename: `${appDir}/logs/${packageJson.name}-debug`, "pattern":".log",alwaysIncludePattern:true}
+    error: { type: 'fileSync', filename: `${appDir}/src/logs/${packageJson.name}-debug`, "pattern":".log",alwaysIncludePattern:true}, 
+    default: { type: 'fileSync', filename: `${appDir}/src/logs/${packageJson.name}-debug`, "pattern":".log",alwaysIncludePattern:true}
   },
   categories: {
     default: { appenders: ['out','default'], level: 'info' },
