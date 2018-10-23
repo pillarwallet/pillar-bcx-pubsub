1113f38ce20d881d19c44f60872c836fd8eb6fc9
Parthasarathy Ramanujam <partha@Parthasarathys-MacBook-Pro.local>
2018-10-23 11:08:00 +0100

    Reset logger configurations to stdout and dateFile

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,15 +1,19 @@
 const log4js = require('log4js');
 const path = require('path');
 const packageJson = require('../../package.json');
-//const appDir = path.dirname(require.main);
 const appDir = require('app-root-path');
 let logger = log4js.getLogger('syslog');
 log4js.configure({
   appenders: {
-    everything: { type: 'file', filename: `${appDir}/src/logs/${packageJson.name}-debug.log`, maxLogSize: 100458760, backups: 3, flags: "rs+" }
+    out: { type: 'stdout' }, 
+    error: { type: 'dateFile', filename: `${appDir}/src/logs/${packageJson.name}-debug`, "pattern":".log",alwaysIncludePattern:true}, 
+    default: { type: 'dateFile', filename: `${appDir}/src/logs/${packageJson.name}-debug`, "pattern":".log",alwaysIncludePattern:true}
   },
   categories: {
-    default: { appenders: [ 'everything' ], level: 'info'}
-  }
+    default: { appenders: ['out','default'], level: 'info' },
+    error: { appenders: ['error'], level: 'error' }
+  },
+  pm2: true,
+  pm2InstanceVar: 'INSTANCE_ID'
 });
 module.exports = logger;
