5fb6d6f27a9b168a61fc93ca8b2acf49d627526c
Parthasarathy Ramanujam <partha@Parthasarathys-MacBook-Pro.local>
2018-11-21 15:20:14 +0000

    Amended common-logger settings

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -5,7 +5,7 @@ const packageJson = require('../../package.json');
 const appDir = appRoot + process.env.LOGS_DIR;
 const level = process.env.LOGS_LEVEL
 const logger = buildLogger(
-    { level, name: packageJson.name + '-log', path: appDir }
+    { level, name: packageJson.name + '-log', path: appDir, logToFile: process.env.NODE_ENV !== 'test' }
 );
 
 module.exports = logger;
