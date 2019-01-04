423aebef1b8d72ab04ee8be2795767c4cc71b3fe
Ricardo Umpierrez <ricardoul@hotmail.com>
2019-01-04 15:50:40 -0300

    Added account tests

--- src/models/__mocks__/accounts_model.js
+++ src/models/__mocks__/accounts_model.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /*
 Copyright (C) 2019 Stiftung Pillar Project
 
@@ -85,3 +86,75 @@ const Accounts = class Accounts {
 
 module.exports.Accounts = Accounts;
 
+=======
+const Accounts = class Accounts {
+  save(callback){
+    Accounts.save(callback)
+  }
+}
+
+  Accounts.find = function(address, callback) {
+    
+    if (callback == null && typeof address == "function")  {
+      const newCallback = address;
+      const newAddress = 'address';
+      newCallback('', [{
+        address: newAddress,
+        FCMIID: 'FCMIID',
+      }]);
+    } else if (callback == null && typeof address == "object"){
+        return{
+          limit: function (params) {
+            return{
+            exec: function(callback){
+              callback(false, [{ "FCMIID": "FCMIID", "address": "address" }])
+            }
+          }
+        }
+      }
+    }
+      else {
+      callback('', [{
+        address,
+        FCMIID: 'FCMIID',
+      }]);
+    }
+  }
+
+  Accounts.findOne = function(address, callback) {
+    if (callback == null) {
+      const newCallback = address;
+      const newAddress = 'address';
+      newCallback('', [{
+        address: newAddress,
+        FCMIID: 'FCMIID',
+      }]);
+    } else {
+      callback('', [{
+        address,
+        FCMIID: 'FCMIID',
+      }]);
+    }
+  }
+
+  Accounts.remove= function(id, callback) {
+    if (callback == null) {
+      const newCallback = id;
+      newCallback('', { result: { n: '9999' } });
+    } else {
+      callback('', { result: { n: '9999' } });
+    }
+  }
+
+  Accounts.update = function(findParams, updateParams, callback) {
+    callback('', 1);
+  }
+
+
+  Accounts.save = function (callback) {
+    callback(false)
+  }
+
+module.exports.Accounts = Accounts;
+
+>>>>>>> Added account tests
