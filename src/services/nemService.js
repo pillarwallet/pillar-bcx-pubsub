3fb5115129201c91c2667cebb228152a3bf814f5
Parthasarathy Ramanujam <partha@Parthasarathys-MacBook-Pro.local>
2019-01-17 12:31:46 +0000

    Added a couple of functions for nem integration

--- src/services/nemService.js
+++ src/services/nemService.js
@@ -1,3 +1,49 @@
 /** @module newService.js */
 require('dotenv').config();
 const nemlib = require('nem-library');
+const UnconfirmedTransactionListener = nemlib.UnconfirmedTransactionListener;
+const NetworkTypes = nemlib.NetworkTypes;
+const BlockchainListener = nemlib.BlockchainListener;
+const hashMaps = require('../utils/hashMaps');
+const protocol = 'NEM';
+
+/**
+ * Establish connection to the geth node
+ */
+function connect() {
+    nemlib.bootstrap(NetworkTypes.TEST_NET);
+}
+module.exports.connect = connect;
+
+/**
+ * Subscribe to NEM WS event corresponding to new pending transactions.
+ */
+function subscribePendingTxn (addresses) {
+    addresses.forEach((address) => {
+        const theAddress = new nemlib.address(address);
+        let unconfirmedTransactionListener = new UnconfirmedTransactionListener().given(theAddress);
+        unconfirmedTransactionListener.subscribe(x => {
+            
+            console.log(x);
+        }, err => {
+            console.log(err);
+        });
+    });
+}
+module.exports.subscribePendingTxn = subscribePendingTxn;
+
+/**
+ * Subscribe to new blocks on NEM blockchain.
+ */
+function subscribeNewBlocks() {
+    let blockchainListener = new BlockchainListener().newBlock();
+
+    blockchainListener.subscribe(x => {
+        console.log(x);
+        //check for confirmed transaction
+    }, err => {
+        console.log(err);
+    });
+}
+module.exports.subscribeNewBlock = subscribeNewBlock;
+
