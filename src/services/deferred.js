010c567edd9bbbc991b463e94017ac414375a681
Ricardo Umpierrez <ricardoul@hotmail.com>
2019-01-25 09:34:46 -0300

    Changed deferred to run in batches

--- src/services/deferred.js
+++ src/services/deferred.js
@@ -13,10 +13,31 @@ function generateList(number) {
     var lista = []
     while (number > 0) {
         lista.push(number);
-        number -= 10000
+        number -= 1000
     }
-    lista.push('earliest')
+    lista.push(0)
     return lista
+}
+
+function decimalToHexString(number) {
+    if (number < 0) {
+        number = 0xFFFFFFFF + number + 1;
+    }
+
+    return "0x"+number.toString(16).toUpperCase();
+}
+
+function saveTransactions(transactions, acc, result){
+    dbServices.dbCollections.historicTransactions.addMultipleTx(transactions).then((transactions) => {
+        logger.debug('deferred.saveDefferedTransactions dbServices.dbCollections.historicTransactions successfully added');
+        acc.status = "deferred_done"
+        result.save((err) => {
+            if (err) {
+                logger.info(`accounts.addAddress DB controller ERROR: ${err}`);
+                reject(err);
+            }
+        });
+    })
 }
 
 
@@ -27,25 +48,14 @@ async function saveDefferedTransactions(result, entry) {
                 if(result){
                     result.addresses.forEach((acc) => {
                         if (acc.status === "deferred"){
-                            ethService.getAllTransactionsForWallet(acc.address).then((transactions) => {
-                                var listOfTrans = generateList(transactions)
-                                for (var i = 0; i < listOfTrans.length -1 ; i++){
-                                    console.log("Esto entro",listOfTrans[i], listOfTrans[i + 1])
-                                    ethService.getAllTransactionsForWallet(acc.address, listOfTrans[i], listOfTrans[i+1]).then((transactions) => {
-                                        var totalTransactions = transactions.length;
-                                        logger.info(`deferred.saveDefferedTransactions: started processing for wallet ${acc.address} and recovered ${totalTransactions}`);
-                                        dbServices.dbCollections.historicTransactions.addMultipleTx(transactions).then((transactions) => {
-                                            logger.debug('deferred.saveDefferedTransactions dbServices.dbCollections.historicTransactions successfully added');
-                                            acc.status = "deferred_done"
-                                            result.save((err) => {
-                                                if (err) {
-                                                    logger.info(`accounts.addAddress DB controller ERROR: ${err}`);
-                                                    reject(err);
-                                                }
-                                            });
-                                        })
-                                    })
-                                }
+                            ethService.getTransactionCountForWallet(acc.address).then((totalTrans) => {
+                                ethService.getLastBlockNumber().then((lastBlock) => {
+                                    logger.info("lastblock is" + lastBlock)
+                                    logger.info("totaltransacions is" + totalTrans)
+                                    var listOfTrans = generateList(lastBlock)
+                                    logger.info("list of trans " + listOfTrans.length )
+                                    getTransactions(listOfTrans, 0, acc, result, totalTrans, [])
+                                })
                             })
                         }
                     })
@@ -59,6 +69,40 @@ async function saveDefferedTransactions(result, entry) {
 
 module.exports.saveDefferedTransactions = saveDefferedTransactions;
 
+
+function getTransactions(listOfTrans, i, acc, result, totalTrans, transList){
+       
+        var toBlock = decimalToHexString(listOfTrans[i + 1])
+        var fromBlock
+        if(i == 0){
+            fromBlock = decimalToHexString(listOfTrans[i])
+        }else{
+            fromBlock = decimalToHexString(listOfTrans[i] + 1 )
+        }
+        logger.info(`deferred.getTransactions: started processing for wallet ${acc.address} and i ${i} fromBlock ${fromBlock} toBlock ${toBlock} length transList ${transList.length}`);
+        ethService.getAllTransactionsForWallet(acc.address, toBlock, fromBlock).then((transactions) => {
+            if (transactions && transactions.length >0){
+              
+                var totalTransactions = transactions.length
+                logger.info(`found transactions ${totalTransactions} `)
+                if (totalTransactions > 0){
+                    transList = transList.concat(transactions)
+                }
+                var transListLength = transList.length
+                if (transListLength >= totalTrans) {
+                    logger.info(`finished, transListLength ${transListLength} totalTrans  ${totalTrans}`)
+                    // saveTransactions(transList, acc, result)
+                    return
+                }else{
+                    getTransactions(listOfTrans, i + 1, acc, result, totalTrans, transList)
+                }
+                logger.info(`deferred.getTransactions: started processing for wallet ${acc.address} and recovered ${totalTransactions} fromBlock ${fromBlock} toBlock ${toBlock} length transList ${transListLength} total trans ${totalTrans}`);
+            }else{
+                getTransactions(listOfTrans, i + 1, acc, result, totalTrans, transList)
+            }
+
+        })
+    }
 
 
 async function launch() {
