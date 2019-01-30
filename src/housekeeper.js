06274c0ff77b0974da9fcfc487d93a50400eefef
Ricardo Umpierrez <ricardoul@hotmail.com>
2019-01-30 10:12:32 -0300

    Removing process.exit

--- src/housekeeper.js
+++ src/housekeeper.js
@@ -253,7 +253,6 @@ function processData(lastId) {
                     client.set('housekeeper',JSON.stringify(entry), redis.print);
                     logger.info(`Housekeeper.processData() - Completed processing ${accounts.length} records.`)
                     this.logMemoryUsage();
-                    process.exit();
 
                 } else {
                     var promises = [];
@@ -273,7 +272,6 @@ function processData(lastId) {
                         client.set('housekeeper',JSON.stringify(entry), redis.print);
                         logger.info(`Housekeeper.processData() - Completed processing ${accounts.length} records.`);
                         this.logMemoryUsage();
-                        process.exit();
                     });
                 }
             });
@@ -318,7 +316,6 @@ async function launch() {
                 if (config.status !== 'completed') {
                     logger.error('HOUSKEEPER FAILED!! PREVIOUS PROCESS IS STILL RUNNING!!');
                     entry = config;
-                    process.exit(0);
                 } else {
                     //the previous run was successful so start process
                     this.cronInit()
@@ -362,7 +359,6 @@ async function init() {
                 if(config.status !== 'completed') {
                     logger.error('HOUSKEEPER FAILED!! PREVIOUS PROCESS IS STILL RUNNING!!');
                     entry = config;
-                    process.exit(0);
                 } else {
                     //the previous run was successful so start process
 
