09238aff1b7c780a8221edccab56294dc9339d28
Ricardo Umpierrez <ricardoul@hotmail.com>
2019-01-31 12:03:30 -0300

    Removed merge lines

--- src/publisher.js
+++ src/publisher.js
@@ -18,18 +18,8 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-<<<<<<< HEAD
-<<<<<<< HEAD
-
 #!/usr/bin/env node*/
-=======
-*/
-#!/usr/bin/env node
->>>>>>> Added copyright disclaimer
-=======
 
-#!/usr/bin/env node*/
->>>>>>> Commented out usr/bin/env node, cause tests to fail
 /** @module publisher.js */
 'use strict';
 const diagnostics = require('./utils/diagnostics');
