9000c0cff07e442d094d6e0e13c5c1f25cd203d5
Parthasarathy Ramanujam <partha@Parthasarathys-MacBook-Pro.local>
2019-02-13 18:28:09 +0000

    Fixed merge conflicts in deferred

--- src/deferred.js
+++ src/deferred.js
@@ -18,13 +18,8 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-<<<<<<< HEAD
-
-#!/usr/bin/env node*/
-=======
 */
 
->>>>>>> master
 'use strict';
 /** @module deferred.js */
 const diagnostics = require('./utils/diagnostics');
